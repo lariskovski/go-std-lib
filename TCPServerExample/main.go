dc93ab6e168f39b1321d74ea6e1f2c0ee54b81b0
lariskovski <larissa@cora.com.br>
2024-11-24 02:29:05 -0300

    feat: Implement HTTP request handling in TCPServerExample with response methods

--- TCPServerExample/main.go
+++ TCPServerExample/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
+	"strings"
+
+	// "io"
 	"net"
 	"time"
 )
@@ -14,7 +17,7 @@ func main() {
 		// Handle error
 		fmt.Println("Error:", err)
 	}
-
+	defer listener.Close()
 	for {
 		// Wait for a connection
 		fmt.Println("waiting for connection")
@@ -23,12 +26,64 @@ func main() {
 			// Handle error
 			fmt.Println("Error:", err)
 		}
-		// Send a message to the connected client
-		io.WriteString(connection, "I see you connected.")
-		fmt.Fprintf(connection, "I see you connected!")
-		// Close the connection
-		connection.Close()
-		// Wait for 5 seconds before accepting the next connection
-		time.Sleep(5 * time.Second)
+		go handle(connection)
 	}
+}
+
+func handle(conn net.Conn){
+	err:= conn.SetDeadline(time.Now().Add(10 * time.Second))
+	if err != nil {
+		fmt.Println("Connection timeout")
+	}
+	defer conn.Close()
+	method, uri := request(conn)
+	if method == "GET" {
+		if uri == "/" {
+			getResponse(conn)
+		}
+	}
+	badResponse(conn)
+}
+
+func request(conn net.Conn) (string, string){
+	i := 0
+	method := ""
+	uri := ""
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan(){
+		line := scanner.Text()
+		// fmt.Println(line)
+		if i == 0 {
+			// request line
+			fields := strings.Fields(line)
+			method = fields[0]
+			uri = fields[1]
+			// fmt.Println("METHOD", method)
+			// fmt.Println("URI", uri)
+			i++
+		}
+		if line == "" {
+			// headers are done
+			break
+		}
+	}
+	return method, uri
+}
+
+func getResponse(conn net.Conn){
+	body := "Hello World"
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/plain\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}
+
+func badResponse(conn net.Conn){
+	body := "BAD REQUEST"
+	fmt.Fprint(conn, "HTTP/1.1 400 BAD REQUEST\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/plain\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
 }
