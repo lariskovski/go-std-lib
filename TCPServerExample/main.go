fef38141a44fae5ac605bc60c0bdf86938d97030
lariskovski <larissa@cora.com.br>
2024-11-23 22:29:30 -0300

    feat: Enhance TCPServerExample with connection handling and comments

--- TCPServerExample/main.go
+++ TCPServerExample/main.go
@@ -7,8 +7,8 @@ import (
 	"time"
 )
 
-
 func main() {
+	// Start listening on TCP port 8080
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		// Handle error
@@ -16,16 +16,19 @@ func main() {
 	}
 
 	for {
+		// Wait for a connection
 		fmt.Println("waiting for connection")
-		connection, err :=listener.Accept()
+		connection, err := listener.Accept()
 		if err != nil {
 			// Handle error
 			fmt.Println("Error:", err)
 		}
+		// Send a message to the connected client
 		io.WriteString(connection, "I see you connected.")
 		fmt.Fprintf(connection, "I see you connected!")
+		// Close the connection
 		connection.Close()
+		// Wait for 5 seconds before accepting the next connection
 		time.Sleep(5 * time.Second)
-
 	}
 }
