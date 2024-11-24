9ab949a84cedc72ff7a46f0d10b57db4e5d4ae62
lariskovski <larissa@cora.com.br>
2024-11-24 12:35:25 -0300

    feat(httpExample): Add HTML form and template rendering for user input

--- httpExample/main.go
+++ httpExample/main.go
@@ -1,14 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
 
 
 type hotdog int
 
 func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world!"))
-}
+	err := r.ParseForm()
+	if err != nil {
+		// Handle error
+		log.Fatal(err)
+	}
 
+	tpl.ExecuteTemplate(w, "index.gohtml", r.Form)
+}
+var tpl *template.Template
+
+func init(){
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
 
 func main() {
 	var d hotdog
