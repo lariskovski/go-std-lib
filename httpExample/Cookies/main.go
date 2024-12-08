b3facc9dcbeaf4d4fa52dcce9845e3b3193457f1
Larissa <37162993+lariskovski@users.noreply.github.com>
2024-12-08 00:12:56 +0000

    feat(httpExample): Refactor cookie handling to count page visits

--- httpExample/Cookies/main.go
+++ httpExample/Cookies/main.go
@@ -1,27 +1,40 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+)
 
 func main(){
-	http.HandleFunc("/", set)
-	http.HandleFunc("/read", read)
+	// Set up the route and handler
+	http.HandleFunc("/", cookie)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	// Start the server on port 8080
 	http.ListenAndServe(":8080", nil)
 }
 
-
-func set(w http.ResponseWriter, r *http.Request){
-	http.SetCookie(w, &http.Cookie{
-		Name: "my-cookie",
-		Value: "some value",
-	})
-	w.Write([]byte("Cookie set"))
-}
-
-func read(w http.ResponseWriter, r *http.Request){
-	c, err := r.Cookie("my-cookie")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
+func cookie(w http.ResponseWriter, r *http.Request){
+	// Initialize counter
+	counter := 0
+	// Try to get the cookie from the request
+	cookie, err := r.Cookie("counter-cookie")
+	if err == http.ErrNoCookie {
+		// If no cookie is found, set a new cookie with value 0
+		http.SetCookie(w, &http.Cookie{
+			Name: "counter-cookie",
+			Value: "0",
+		})
 	}
-	w.Write([]byte("Your cookie: " + c.Value))
+
+	// Convert cookie value to integer
+	counter, _ = strconv.Atoi(cookie.Value)
+	// Increment the counter
+	counter++
+	// Update the cookie value
+	cookie.Value = strconv.Itoa(counter)
+
+	// Set the updated cookie in the response
+	http.SetCookie(w, cookie)
+	// Write the response to the client
+	w.Write([]byte("You have visited this page " + strconv.Itoa(counter) + " times"))
 }
