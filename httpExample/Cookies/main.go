e3192f764e68de4b1ce26f314f428c8224dfe87b
Larissa <37162993+lariskovski@users.noreply.github.com>
2024-12-08 02:11:56 +0000

    feat(httpExample): Enhance cookie handling to initialize and increment visit counter

--- httpExample/Cookies/main.go
+++ httpExample/Cookies/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -10,31 +12,35 @@ func main(){
 	http.HandleFunc("/", cookie)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	// Start the server on port 8080
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Server is running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func cookie(w http.ResponseWriter, r *http.Request){
 	// Initialize counter
-	counter := 0
+	var counter int
 	// Try to get the cookie from the request
 	cookie, err := r.Cookie("counter-cookie")
 	if err == http.ErrNoCookie {
 		// If no cookie is found, set a new cookie with value 0
 		http.SetCookie(w, &http.Cookie{
 			Name: "counter-cookie",
-			Value: "0",
+			Value: "1",
 		})
+		counter = 1
+	} else if err != nil {
+		// If there is an error, write the error to the response
+		w.Write([]byte(err.Error()))
+		return
+	} else {
+		counter, _ = strconv.Atoi(cookie.Value)
+		counter++
+		fmt.Println("Incremented counter: ", counter)
+		cookie.Value = strconv.Itoa(counter)
+	
+		// Set the updated cookie in the response
+		http.SetCookie(w, cookie)
 	}
-
-	// Convert cookie value to integer
-	counter, _ = strconv.Atoi(cookie.Value)
-	// Increment the counter
-	counter++
-	// Update the cookie value
-	cookie.Value = strconv.Itoa(counter)
-
-	// Set the updated cookie in the response
-	http.SetCookie(w, cookie)
 	// Write the response to the client
 	w.Write([]byte("You have visited this page " + strconv.Itoa(counter) + " times"))
 }
